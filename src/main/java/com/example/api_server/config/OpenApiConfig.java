5ff10b97af14b22c00a6bf49a4bfa328e3deb88d
ho0116 <luminous_dawn@icould.com>
2024-08-01 11:43:46 +0900

    first commit

---
+++ src/main/java/com/example/api_server/config/OpenApiConfig.java
@@ -0,0 +1,18 @@
+package com.example.api_server.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.*;
+
+@Configuration
+public class OpenApiConfig {
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("User Management API")
+                        .version("1.0")
+                        .description("This is a sample Spring Boot RESTful service using springdoc-openapi.")
+                );
+    }
+}
